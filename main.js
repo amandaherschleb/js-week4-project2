e6667bd7329836134da5511cbb1aebe9a7fb5d60
Amanda Herschleb <amanda.herschleb@gmail.com>
2016-08-11 19:41:53 -0600

    completed project

--- main.js
+++ main.js
@@ -4,49 +4,75 @@
 ** Week 4 - Project 2**
 *** jQuery Mania ***
 
-
 =======================================================
 */
 
 // Add your JS here.
 
 
-$(document).ready(function() {  
-    
-    
-//1. 
-   
-   
-//2.  
-    
-    
-//3. 
-   
-	    
+$(document).ready(function() {
+
+
+//1.
+$('.alertme').click(function(){
+  alert('jQuery seems easy so far!');
+});
+
+//2.
+$('.clickme').click(function(){
+  $(this).html('I was clicked');
+});
+
+
+//3.
+$('.addStyle').click(function(){
+  $('button').addClass('buttonStyle');
+});
+
 //4.
- 
-   
-//5. 
+$('.addDifferentStyle').click(function(){
+  $(this).addClass('addUniqueStyle');
+});
 
-	
+//5.
+$('.removeBtn').click(function(){
+  $('button').removeClass();
+});
+
+
 //6.
-	
-	
+$('.clickToggle').click(function(){
+  $(this).toggleClass('buttonStyle');
+});
+
+
 //7.
+$('.squareBtn').click(function(){
+  $('.square').hide();
+});
 
-	
-	
+//.toggle() would hide then show
+
 //8.
-
+$('.circle').click(function(){
+  $(this).hide();
+});
 
 
 //9.
+$('.toggleRedSquare').click(function(){
+  $('.redSquare').fadeOut(750, function(){
+    $('.redSquare').fadeIn(750);
+  });
+});
+
+
+//Alternate way:
+// $('.toggleRedSquare').click(function(){
+//   $('.redSquare').fadeOut(750);
+//   $('.redSquare').fadeIn(750);
+// });
 
 
 
-});  // Close: $(document).ready(function() { 
-	
-	
-
-	
-	
+});
